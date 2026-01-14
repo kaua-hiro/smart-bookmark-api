7b414251641e1dd4ed9fba2a6514cabee80688c1
kaua-hiro <kauahiro2004@hotmail.com>
2026-01-14 10:11:22 -0300

    criando conexao com banco e model do link

---
+++ models.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+class Link(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    url: str
+    title: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.now)
