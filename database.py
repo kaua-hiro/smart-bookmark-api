8e87473a66f83c542c139a522a73a7f22d286af5
kaua-hiro <kauahiro2004@hotmail.com>
2026-01-14 10:21:03 -0300

    refactor: movendo credenciais para variaveis de ambiente

--- database.py
+++ database.py
@@ -1,14 +1,22 @@
 from sqlmodel import SQLModel, create_engine, Session
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "mssql+pyodbc://SEU_USUARIO:SUA_SENHA@SEU_SERVIDOR/NOME_DO_BANCO?driver=ODBC+Driver+17+for+SQL+Server"
+# Carrega as variaveis do arquivo .env
+load_dotenv()
 
-# echo=True ajuda a ver o SQL gerado no terminal (bom pra aprender)
-engine = create_engine(DATABASE_URL, echo=False)
+# Pega a string de conexao segura
+database_url = os.getenv("DB_CONNECTION_STRING")
+
+# Verificacao de seguranca (caso esqueca de criar o .env)
+if not database_url:
+    raise ValueError("A variavel DB_CONNECTION_STRING não foi definida no .env")
+
+engine = create_engine(database_url)
 
 def get_session():
     with Session(engine) as session:
         yield session
 
 def create_db_and_tables():
-    # Cria as tabelas baseadas nos Models
     SQLModel.metadata.create_all(engine)
