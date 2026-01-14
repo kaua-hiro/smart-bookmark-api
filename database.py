021df8176f988282105cb9b95bb0af0624a6295e
kaua-hiro <kauahiro2004@hotmail.com>
2026-01-14 10:17:11 -0300

    configuracao de conexao sql server com sqlmodel

--- database.py
+++ database.py
@@ -1,14 +1,14 @@
 from sqlmodel import SQLModel, create_engine, Session
 
+DATABASE_URL = "mssql+pyodbc://SEU_USUARIO:SUA_SENHA@SEU_SERVIDOR/NOME_DO_BANCO?driver=ODBC+Driver+17+for+SQL+Server"
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-engine = create_engine(sqlite_url)
+# echo=True ajuda a ver o SQL gerado no terminal (bom pra aprender)
+engine = create_engine(DATABASE_URL, echo=False)
 
 def get_session():
     with Session(engine) as session:
         yield session
 
 def create_db_and_tables():
+    # Cria as tabelas baseadas nos Models
     SQLModel.metadata.create_all(engine)
