e84457a026d929010a6d3cefb2380476b0b4de39
kaua-hiro <kauahiro2004@hotmail.com>
2026-01-14 10:18:49 -0300

    setup final do main e registro de rotas

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlmodel import Session, select
-from database import create_db_and_tables, get_session
-from models import Link
-from services import get_page_title
+from fastapi import FastAPI
+from database import create_db_and_tables
+from controllers import router as link_router
 
-app = FastAPI()
+app = FastAPI(title="Smart Bookmark API - MVC Version")
 
-# Cria as tabelas quando o app inicia
 @app.on_event("startup")
 def on_startup():
+    # Garante que as tabelas existam no SQL Server ao iniciar
     create_db_and_tables()
 
-@app.post("/links/", response_model=Link)
-async def create_link(link_data: Link, session: Session = Depends(get_session)):
-    # Primeiro pega o titulo automatico
-    title = await get_page_title(link_data.url)
-    link_data.title = title
+# Registra as rotas (Controllers)
+app.include_router(link_router)
 
-    session.add(link_data)
-    session.commit()
-    session.refresh(link_data)
-    return link_data
-
-@app.get("/links/", response_model=list[Link])
-def read_links(session: Session = Depends(get_session)):
-    links = session.exec(select(Link)).all()
-    return links
+@app.get("/")
+def root():
+    return {"message": "API Online. Acesse /docs para testar."}
